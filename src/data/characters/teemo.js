1912a1e52ef243818be827188d81456fd9aad2dc
ConnorBarber <cb386316@gmail.com>
2026-02-07 15:18:35 -0500

    Teemo pics and notations

--- src/data/characters/teemo.js
+++ src/data/characters/teemo.js
@@ -22,14 +22,14 @@ export const teemo = {
       title: 'Key Moves',
       description: [],
       moves: [
-        { numericNotation: 'L(LL)', notation: 'Projectile normal that can be used for: grounded keepout, dart badge, and combos', notationImage: '/key-moves-notation/teemo/teemo1.png', description: 'UNDER CONSTRUCTION', video: 'videos/teemo/teemo1.mp4' },
-        { numericNotation: '2L', notation: 'Teemo\'s fastest button(NOTE: ONLY WORKS POINT BLANK), and a low', notationImage: '/key-moves-notation/teemo/teemo2.png', description: 'UNDER CONSTRUCTION', video: 'videos/teemo/teemo2.mp4' },
-        { numericNotation: '5M', notation: 'Teemo\'s fastest button with good range(8 frames + better range than 2L), and a combo starter', notationImage: '/key-moves-notation/teemo/teemo3.png', description: 'UNDER CONSTRUCTION', video: 'videos/teemo/teemo3.mp4' },
-        { numericNotation: '2M', notation: 'Forward advancing low, and combo starter(NOTE: First hit is mid and 2 follow ups are low)', notationImage: '/key-moves-notation/teemo/teemo4.png', description: 'UNDER CONSTRUCTION', video: 'videos/teemo/teemo4.mp4' },
-        { numericNotation: '2H', notation: 'Universal Anti Air', notationImage: '/key-moves-notation/teemo/teemo5.png', description: 'UNDER CONSTRUCTION', video: 'videos/teemo/teemo5.mp4' },
-        { numericNotation: 'j.L', notation: 'Diagonal poke tool, used for keepout, combos. and approach', notationImage: '/key-moves-notation/teemo/teemo5.png', description: 'UNDER CONSTRUCTION', video: 'videos/teemo/teemo5.mp4' },
-        { numericNotation: 'S2 (j.S2)', notation: 'Shroom traps used to build mushroom meter, combo, camp/zoning keepout, and oki', notationImage: '/key-moves-notation/teemo/teemo5.png', description: 'UNDER CONSTRUCTION', video: 'videos/teemo/teemo5.mp4' },
-        { numericNotation: 'j.M', notation: 'Multi-hitting jump normal. Very active and deceptively big hitbox beneath and very easy to confirm.', notationImage: '/key-moves-notation/teemo/teemo5.png', description: 'UNDER CONSTRUCTION', video: 'videos/teemo/teemo5.mp4' },
+        { numericNotation: 'L(LL)', notation: 'Projectile normal that can be used for: grounded keepout, dart badge, and combos', notationImage: '/key-moves-notation/teemo/teemo1.png', description: 'Projectile normal that can be used for: grounded keepout, dart badge, and combos	', video: 'videos/teemo/teemo1.mp4' },
+        { numericNotation: '2L', notation: 'Teemo\'s fastest button(NOTE: ONLY WORKS POINT BLANK), and a low', notationImage: '/key-moves-notation/teemo/teemo2.png', description: 'Teemo\'s fastest button(NOTE: ONLY WORKS POINT BLANK), and a low	', video: 'videos/teemo/teemo2.mp4' },
+        { numericNotation: '5M', notation: 'Teemo\'s fastest button with good range(8 frames + better range than 2L), and a combo starter', notationImage: '/key-moves-notation/teemo/teemo3.png', description: 'Teemo\'s fastest button with good range(8 frames + better range than 2L), and a combo starter	', video: 'videos/teemo/teemo3.mp4' },
+        { numericNotation: '2M', notation: 'Forward advancing low, and combo starter(NOTE: First hit is mid and 2 follow ups are low)', notationImage: '/key-moves-notation/teemo/teemo4.png', description: 'Forward advancing low, and combo starter(NOTE: First hit is mid and 2 follow ups are low)', video: 'videos/teemo/teemo4.mp4' },
+        { numericNotation: '2H', notation: 'Universal Anti Air', notationImage: '/key-moves-notation/teemo/teemo5.png', description: 'Universal Anti Air', video: 'videos/teemo/teemo5.mp4' },
+        { numericNotation: 'j.L', notation: 'Diagonal poke tool, used for keepout, combos. and approach', notationImage: '/key-moves-notation/teemo/teemo6.png', description: 'Diagonal poke tool, used for keepoutl, combos. and approach ', video: 'videos/teemo/teemo6.mp4' },
+        { numericNotation: 'S2 (j.S2)', notation: 'Shroom traps used to build mushroom meter, combo, camp/zoning keepout, and oki', notationImage: '/key-moves-notation/teemo/teemo7.png', description: 'Shroom traps used to build mushroom meter, combo, camp/zoning keepout, and oki	', video: 'videos/teemo/teemo7.mp4' },
+        { numericNotation: 'j.M', notation: 'Multi-hitting jump normal. Very active and deceptively big hitbox beneath and very easy to confirm.', notationImage: '/key-moves-notation/teemo/teemo8.png', description: 'Multi-hitting jump normal. Very active and deceptively big hitbox beneath and very easy to confirm.', video: 'videos/teemo/teemo8.mp4' },
       ]
     },
     {
@@ -37,17 +37,19 @@ export const teemo = {
       title: 'Combos',
       description: [],
       combos: [
-        { purpose: 'Easy BnB', numericNotation: 'L M H 2H j.M j.H j.S1 66 j.H 2T', notationImage: '/combo-notation/teemo/teemo-combo-1.png', video: 'videos/teemo/teemo-combo-1.mp4' },
-        { purpose: 'BnB', numericNotation: 'M > HH > j.LLL > j.d.M(1 hit only) > M > H > j.LLL > j.2MMM > j.2H > 3H > S1+L', notationImage: '/combo-notation/teemo/teemo-combo-1.png', video: 'videos/teemo/teemo-combo-2.mp4' },
-        { purpose: 'Corner Carry', numericNotation: 'HM > HH > {2H} > j.6S1 > M > {2H} > j.6S1 > M > {2H} > [6S1] > Super(S1 super in the open, S2 on wall slump)', notationImage: '/combo-notation/teemo/teemo-combo-2.png', video: 'videos/teemo/teemo-combo-2.mp4' },
-        { purpose: 'Tag Launcher', numericNotation: 'M > HH > j.LLL > j.d.M(1 hit only) > M > H > j.LLL > j.2MMM > j.2H > 3H > T', notationImage: '/combo-notation/teemo/teemo-combo-3.png', video: 'videos/teemo/teemo-combo-3.mp4' },
-        { purpose: 'Jump In', numericNotation: 'j.L > j.M > M > HH > j.LLL > j.d.M(1 hit only) > M > H > j.LLL > j.2MMM > j.2H > 3H > S1+L', notationImage: '/combo-notation/teemo/teemo-combo-4.png', video: 'videos/teemo/teemo-combo-4.mp4' },
-        { purpose: 'Corner Combo', numericNotation: 'j.L > j.M > M > HH > j.LLL > j.d.M(1 hit only) > M > H > j.LLL > j.2MMM > j.2H > 3H > S1+L', notationImage: '/combo-notation/teemo/teemo-combo-4.png', video: 'videos/teemo/teemo-combo-4.mp4' },      
+        { purpose: 'Easy BnB', numericNotation: 'M > H > 2H > j.LLL > j.MMM > j.2H > 3H > S1+L', notationImage: '/combo-notation/teemo/teemo-combo-1.png', video: 'videos/teemo/teemo-combo-1.mp4' },
+        { purpose: 'BnB', numericNotation: 'M > HH > j.LLL > j.d.M(1 hit only) > H > j.LLL > j.2MMM > j.2H > 3H > S1+L', notationImage: '/combo-notation/teemo/teemo-combo-2.png', video: 'videos/teemo/teemo-combo-2.mp4' },
+        { purpose: 'Corner Carry/Shroom Loop', numericNotation: 'M > HH > {2H} > j.6S2 > M > {2H} > j.6S2 > M > {2H} > [6S1] > Super(S1 super in the open, S2 on wall slump)', notationImage: '/combo-notation/teemo/teemo-combo-3.png', video: 'videos/teemo/teemo-combo-2.mp4' },
+        { purpose: 'Tag Launcher', numericNotation: 'M > HH > j.LLL > j.d.M(1 hit only) > M > H > j.LLL > j.2MMM > j.2H > 3H > T', notationImage: '/combo-notation/teemo/teemo-combo-4.png', video: 'videos/teemo/teemo-combo-4.mp4' },
+        { purpose: 'Jump In', numericNotation: 'j.L > j.M > M > HH > j.LLL > j.d.M(1 hit only) > H > j.LLL > j.2MMM > j.2H > 3H > S1+L', notationImage: '/combo-notation/teemo/teemo-combo-5.png', video: 'videos/teemo/teemo-combo-5.mp4' },
+        { purpose: 'Corner Combo/Oki', numericNotation: 'M > HH > {2H} > j.5S1 > M > {2H} > j.5S1 > 6S1 > S2+L > 2S2 > j.2H	', notationImage: '/combo-notation/teemo/teemo-combo-6.png', video: 'videos/teemo/teemo-combo-6.mp4' },      
       ],
       comboEnders: [
-        { purpose: 'Oki', numericNotation: 'j.H 2S1 2T', notationImage: '/combo-notation/teemo/teemo-ender-1.png', video: 'videos/teemo/teemo-ender-1.mp4' },
-        { purpose: 'Poison Stack', numericNotation: 'S2 j.H j.S1 2T', notationImage: '/combo-notation/teemo/teemo-ender-2.png', video: 'videos/teemo/teemo-ender-2.mp4' },
-        { purpose: 'Max Dmg', numericNotation: 'j.H j.S2 TAG', notationImage: '/combo-notation/teemo/teemo-ender-3.png', video: 'videos/teemo/teemo-ender-3.mp4' },
+        { purpose: 'Oki', numericNotation: 'j.H', notationImage: '/combo-notation/teemo/teemo-ender-1.png', video: 'videos/teemo/teemo-ender-1.mp4' },
+        { purpose: 'Limit Strike	', numericNotation: 'H or 6S1', notationImage: '/combo-notation/teemo/teemo-ender-2.png', video: 'videos/teemo/teemo-ender-2.mp4' },
+        { purpose: 'Damage', numericNotation: 'S1+L', notationImage: '/combo-notation/teemo/teemo-ender-3.png', video: 'videos/teemo/teemo-ender-3.mp4' },
+        {purpose: 'Hard Tag', numericNotation: 'S2+L > Hard Tag', notationImage: '/combo-notation/teemo/teemo-ender-4.png', video: 'videos/teemo/teemo-ender-4.mp4' },
+        {purpose: 'Tag Launcher', numericNotation: '3H for OTG, 6S1 in corner > T', notationImage: '/combo-notation/teemo/teemo-ender-5.png', video: 'videos/teemo/teemo-ender-5.mp4' },
       ]
     },
     {
@@ -56,23 +58,75 @@ export const teemo = {
       description: '',
       teams: [
         {
-          pairing: 'Ekko',
+          pairing: 'Yasuo',
           fuseType: 'Double Down',
-          explanation: 'Teemo super clears the screen for Ekko to get in. Great synergy for rushdown.',
+          explanation: 'yasuo covers teemo\'s bad matchups(jinx, illaoi, caitlyn), and they can double down super together for both mix and dmg(ex: tornado first 3 hits into megashroom)',
           videos: [
             { id: 'F3x1-NNy6h4' },
           ]
         },
         {
-          pairing: 'Caitlyn',
-          fuseType: 'Freestyle',
-          explanation: 'Double zoner team with excellent trap coverage. Mushrooms + Traps = nightmare.',
+          pairing: 'Illaoi',
+          fuseType: '2x Assit or Double Down',
+          explanation: 'These two characters both have great 2x Assist utitility. Illaoi enhances Teemo\'s corner pressure with intense lockdown, and her S2 super or super assist + megashroom can lock down the field for keepout',
+          videos: [
+            { id: 'gCDtZgLubqw' },
+          ]
+        },
+        {
+          pairing: 'Jinx',
+          fuseType: 'Double Down or Freestyle',
+          explanation: 'This team is not the strongest team, but if you want full zoning abilities, it grants all angles of coverage. Jinx gives teemo full screen zoning power, and additional items to clutter the field for keepout.',
           videos: [
             { id: 'gCDtZgLubqw' },
           ]
         },
       ]
     },
+      {
+        id: 'top_players',
+        title: 'Top Players',
+        players: [
+          {
+            name: 'Zando',
+            description: '',
+            socials: {
+              twitter: 'https://twitter.com/Zando',
+              youtube: 'https://www.youtube.com/@Zando',
+              twitch: 'http://www.twitch.tv/Zando'
+            },
+            highlights: [
+              { id: 'EZ2mbmn7nvM' }
+            ]
+          },
+          {
+            name: 'Blaizzy',
+            description: '',
+            socials: {
+              twitch: 'https://www.twitch.tv/blaizzy'
+            },
+            highlights: [
+              { id: 'S3CxJx9olO0' }
+            ]
+          },
+        ]
+      },
+      {
+        id: 'guides',
+        title: 'Guides',
+        videos: [
+          { 
+            id: 'Ie8m5VuaXGA', 
+          },
+          { 
+            id: 'tZLOSgLZFE4', 
+          },
+          { 
+            id: 'tZd52GhWjgA', 
+          },
+        ],
+      },
+    ],
     // {
     //   id: 'matchups',
     //   title: 'Matchups',
@@ -109,5 +163,4 @@ export const teemo = {
     //     },
     //   }
     // },
-  ]
 }
